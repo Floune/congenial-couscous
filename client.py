53be615f6f2077362c638f3c71b43e8e908dad86
floune <rfournier@simplonprod.co>
2021-10-25 17:56:26 +0200

    commands

--- client.py
+++ client.py
@@ -5,17 +5,21 @@ from nickname import randoum
 from curses import wrapper
 import signal
 import time
-import asyncio
+import sys
 from desktop_notifier import DesktopNotifier, Urgency, Button
+import requests
+import vlc
+
  
 def handler(signum, frame):
-    client.send("____deco".encode('ascii'))
+    client.send("____deco".encode('utf-8'))
     curses.endwin()
     sys.exit(1)
  
 signal.signal(signal.SIGINT, handler)
 q = queue.Queue()
 notify = DesktopNotifier()
+p = vlc.MediaPlayer("https://listen.nolife-radio.com/stream")
 
 messages = []
 currentMsg = ""
@@ -24,15 +28,15 @@ nicknames = []
 
 nickname = randoum()
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('', 5556))
+client.connect(('localhost', 8766))
 
 
 def receive():
     while True:
         try:
-            message = client.recv(1024).decode('ascii')
+            message = client.recv(1024).decode('utf-8')
             if message == 'NICKNAME':
-                client.send(nickname.encode('ascii'))
+                client.send(nickname.encode('utf-8'))
             else:
                 q.put(message)
 
@@ -51,25 +55,26 @@ def gui():
         m = q.get()
         handleMessages(m)
         updateGui(win)
-    return win
 
        
 def updateGui(win):
-    notify.send_sync(
-        title="prout",
-        message="nouveau message ultra secret",
-        urgency=Urgency.Critical,
-        buttons=[
-            Button(title="j'ai vu, fous moi la paix"),
-        ],
-    ),
+
+    # notify.send_sync(
+    #     title="prout",
+    #     message="nouveau message ultra secret",
+    #     urgency=Urgency.Critical,
+    #     buttons=[
+    #         Button(title="j'ai vu, fous moi la paix"),
+    #     ],
+    # ),
+
     win.move(0, 0)
     win.clear()
     win.refresh()
 
     for i, m in enumerate(messages):
         arr = m.split("::::")
-        if len(arr) > 1:
+        if len(arr) > 1 and len(arr[1]) > 0:
             colorIndex = (ord(arr[0][0]) % 5) + 1
             win.addstr(i, 0, arr[0], curses.color_pair(colorIndex))
             win.addstr(i, len(arr[0]) + 2, arr[1])
@@ -84,19 +89,34 @@ def handleMessages(message):
     colorSequence.append(random.choice(range(1, 5)))
     messages.append(message)
 
-
+def handleCommand(c):
+    if (c[1:] == "joke"):
+        response = requests.get("https://api.chucknorris.io/jokes/random")
+        client.send('{}::::{}'.format(nickname, response.json()['value']).encode("utf8"))
+    elif (c[1:] == "shrug"):
+        client.send('{}::::{}'.format(nickname, "¯\\_(ツ)_/¯").encode("utf8"))
+    elif (c[1:] == "nolife"):
+        p.play() 
+    elif (c[1:] == "stop"):
+        p.stop()
+    else:
+        client.send('{}'.format(c).encode("utf8"))
 
 def write():
     win = curses.newwin(1, 200, 23, 0)
-    win.clear()
-    win.refresh()
     win.keypad(True)
     curses.echo()
+    win.clear()
+    win.refresh()
     while True:
         win.move(0, 0)
         c = win.getstr(0, 0)
-        message = '{}::::{}'.format(nickname, c.decode("utf8").encode('ascii', 'ignore').decode('ascii'))
-        client.send(message.encode('ascii'))
+        decoded = c.decode('utf-8')
+        if len(decoded) > 1 and decoded[0] == "/":
+            handleCommand(decoded)
+        else:
+            message = '{}::::{}'.format(nickname, decoded)
+            client.send(message.encode('utf-8'))
         win.clear()
         win.refresh()
 
@@ -106,11 +126,12 @@ def main(stdscr):
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE),
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_GREEN),
     curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_RED),
-    curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_YELLOW),
+    curses.init_pair(5, curses.COLOR_BLACK, curses.COLOR_YELLOW),
     curses.init_pair(6, curses.COLOR_WHITE, curses.COLOR_MAGENTA),
 
     receive_thread = threading.Thread(target=receive)
     receive_thread.start()
+
     write_thread = threading.Thread(target=write)
     write_thread.start()
     
--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 import socket, threading
 import sys
+import signal
 host = ''                                                    
-port = 8767                                                           
+port = 8766                                                           
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)             
 server.bind((host, port))                                             
 server.listen()
+ 
+def handler(signum, frame):
+    sys.exit(1)
+ 
+signal.signal(signal.SIGINT, handler)
 
 
 clients = []
@@ -19,12 +25,15 @@ def handle(client):
         try:        
             message = client.recv(1024)
             print(message)
-            if message.decode('ascii') == "____deco":
+            if message.decode('utf-8') == "____deco":
                 sys.exit()
                 deco(client)
+            elif message.decode('utf-8')[0] == "/":
+                handleCommand(message.decode('utf-8')[1:], client)
             else:
                 broadcast(message)
-        except:        
+        except:   
+
             deco(client)
             break
 
@@ -34,12 +43,12 @@ def connect():
         client, address = server.accept()     
         
         #demande de pseudo
-        client.send('NICKNAME'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
+        client.send('NICKNAME'.encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
         nicknames.append(nickname)
         clients.append(client)
         print("{} connected as {}".format(str(address), nickname))
-        broadcast("{}::::joined!".format(nickname).encode('ascii'))
+        broadcast("{}::::joined!".format(nickname).encode('utf-8'))
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
@@ -49,8 +58,15 @@ def deco(client):
     clients.remove(client)
     client.close()
     nickname = nicknames[index]
-    broadcast('{}::::left!'.format(nickname).encode('ascii'))
-    print('{} left!'.format(nickname).encode('ascii'))
+    broadcast('{}::::left!'.format(nickname).encode('utf-8'))
+    print('{} left!'.format(nickname).encode('utf-8'))
     nicknames.remove(nickname)
 
+def handleCommand(c, client):
+    if (c == "help"):
+        client.send('system::::/joke /shrug /nolife /stop'.encode('utf-8'))
+    else:
+        client.send('system:::: /help pour la liste des commandes'.encode('utf-8'))
+
 connect()
+
